db64e9e2fe6e89cff06e62e1cfd3ad160026b80a
Arman Alam <armanalam78578@gmail.com>
2024-12-11 11:13:26 +0530

    next auth functionality setup

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,17 @@
+import client from "@/lib/db";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google";
+
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  adapter: MongoDBAdapter(client)
+})
+
+export { handler as GET, handler as POST }
