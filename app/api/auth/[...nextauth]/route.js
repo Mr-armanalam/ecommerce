a188e8367c8f5a5d6bb716949350b4d018fe666e
Arman Alam <armanalam78578@gmail.com>
2024-12-20 16:11:25 +0530

    envirenmental variable changed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,41 +1,45 @@
 import client from "@/lib/db";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
-import NextAuth, { getServerSession } from "next-auth"
+import NextAuth, { getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-
-const adminEmails = ['armanalam78578@gmail.com'] ;
-const anotherEmails = ['armanalam91174@gmail.com'];
+const adminEmails = ["armanalam78578@gmail.com"];
+const anotherEmails = ["armanalam91174@gmail.com"];
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
+    }),
   ],
   adapter: MongoDBAdapter(client),
   callbacks: {
-    session: ({session, token, user}) => {
+    session: ({ session, token, user }) => {
       // console.log({session, token, user});
-      if (adminEmails.includes(session?.user?.email)){
+      if (adminEmails.includes(session?.user?.email)) {
         return session;
-      } else if (anotherEmails.includes(session?.user?.email)){ /////////////////// additional ////////////////
+      } else if (anotherEmails.includes(session?.user?.email)) {
+        /////////////////// additional ////////////////
         return session;
-      }     
-      else {
+      } else {
         return false;
       }
-    }
-  }
-}
+    },
+  },
+};
 
 const handler = NextAuth(authOptions);
 
-export async function isAdminRequest(){
+export async function isAdminRequest() {
   const session = await getServerSession(authOptions);
-  if (!(adminEmails.includes(session?.user?.email ) || anotherEmails.includes(session?.user?.email))){
-    throw {message: 'not an admin'};
+  if (
+    !(
+      adminEmails.includes(session?.user?.email) ||
+      anotherEmails.includes(session?.user?.email)
+    )
+  ) {
+    throw { message: "not an admin" };
   }
 }
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import path from "path";
 import { writeFile, unlink } from "fs/promises";
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 import { isAdminRequest } from "../auth/[...nextauth]/route";
 
 export const config = {
@@ -11,27 +11,34 @@ export const config = {
 };
 
 cloudinary.config({
-  cloud_name:process.env.CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret:process.env.CLOUDINARY_SECRET
+  NEXT_PUBLIC_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUD_NAME,
+  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_KEY,
+  api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_SECRET,
 });
 
 export async function POST(req) {
   await isAdminRequest();
 
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
     const formData = await req.formData();
-    const file = formData.get('file');
+    const file = formData.get("file");
     if (!file) {
-      return NextResponse.json({ error: "No files received." }, { status: 400 });
+      return NextResponse.json(
+        { error: "No files received." },
+        { status: 400 }
+      );
     }
-    
+
     const buffer = Buffer.from(await file.arrayBuffer());
     // const originalFilename = file.name.replaceAll(" ", "_");
     // const newFilename = `${path.parse(originalFilename).name}_${Date.now()}${path.extname(originalFilename)}`;
-    const newFilename = `product_${Date.now()}`   
-    const localFilePath = path.join(process.cwd(), "public/assets/", newFilename);
-    
+    const newFilename = `product_${Date.now()}`;
+    const localFilePath = path.join(
+      process.cwd(),
+      "public/assets/",
+      newFilename
+    );
+
     try {
       await writeFile(localFilePath, buffer);
       const result = await cloudinary.uploader.upload(localFilePath, {
@@ -40,12 +47,15 @@ export async function POST(req) {
       });
 
       await unlink(localFilePath);
-      return NextResponse.json({ message: "Success", url: result.secure_url }, { status: 201 });
+      return NextResponse.json(
+        { message: "Success", url: result.secure_url },
+        { status: 201 }
+      );
     } catch (error) {
-      console.error('Error uploading file:', error);
-      return NextResponse.json( error.message , { status: 500 });
+      console.error("Error uploading file:", error);
+      return NextResponse.json(error.message, { status: 500 });
     }
   } else {
-    return NextResponse.json('Method not allowed', { status: 405 });
+    return NextResponse.json("Method not allowed", { status: 405 });
   }
 }
