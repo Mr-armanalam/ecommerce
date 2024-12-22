51e66565ec87cfd88ab0025f1abae69c17e9dd80
Arman Alam <armanalam78578@gmail.com>
2024-12-22 17:31:58 +0530

    package json file added with material ui

--- app/(root)/(home)/page.jsx
+++ app/(root)/(home)/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import LineChart from "@/components/client/LineCharts";
 import { useSession } from "next-auth/react";
 import Image from "next/image";
 import React from "react";
@@ -7,22 +8,26 @@ export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <div className="text-blue-900 flex justify-between">
+    <div className="text-blue-900 ">
       { session?.user &&
         <>
-          <h2>
-            Hello, <b>{session?.user.name}</b>
-          </h2>
-          <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
-            <Image
-              src={session?.user?.image || ""}
-              width={24}
-              height={24}
-              alt="avatar"
-              className="w-6 h-6"
-            />
-            <span className=" px-2">{session?.user?.name}</span>
+          <div className="flex justify-between">
+            <h2>
+              Hello, <b>{session?.user.name}</b>
+            </h2>
+            <div className="flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden">
+              <Image
+                src={session?.user?.image || ""}
+                width={32}
+                height={32}
+                alt="avatar"
+                className="w-8 h-8"
+              />
+              <span className="pt-1 text-gray-800 font-semibold px-2">{session?.user?.name}</span>
+            </div>
           </div>
+
+          <LineChart />
         </>
       }
     </div>
